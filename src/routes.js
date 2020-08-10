6bb817c1470330d70c6ff3ae45d3e89562512812
Matheus Piffer <matheus.piffer.bc@gmail.com>
2020-08-10 19:01:17 -0300

    search routes

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,13 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const products = require('./app/controllers/products')
 const home = require('./app/controllers/home')
-routes.get('/', home.index)
+const search = require('./app/controllers/search')
 
+//home
+routes.get('/', home.index)
+//search
+routes.get('/products/search', search.index)
+//products
 routes.get('/products/create', products.create)
 routes.get('/products/:id', products.show)
 routes.get('/products/:id/edit', products.edit)
@@ -12,7 +17,7 @@ routes.post('/products', multer.array('photos', 6), products.post)
 routes.put('/products', multer.array('photos', 6), products.put)
 routes.delete('/products', products.delete)
 
-//
+// alias
 routes.get('/ads/create', (req, res) => {
     return res.redirect('/products/create')
 })
